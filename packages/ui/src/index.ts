ed61e67f4f949688b49931a0920a3e83f0523d61
admin <lola@MacBook-Pro-lola.local>
2026-04-02 18:52:07 +0500

    auto: update index.ts

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,4 @@
+export { cn } from './utils/cn';
+export { formatUZS } from './utils/format-currency';
+export { formatPhone } from './utils/format-phone';
+export { formatDate, formatTime, formatWeekday, formatDateTime } from './utils/format-date';
