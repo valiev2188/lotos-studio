eab43b787a484ef2f3af46cebbabdba208f5a861
admin <lola@MacBook-Pro-lola.local>
2026-04-02 14:39:22 +0500

    auto: update react.js

---
+++ packages/config/eslint/react.js
@@ -0,0 +1,24 @@
+import base from './base.js';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+
+export default [
+  ...base,
+  {
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': reactHooksPlugin,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+];
